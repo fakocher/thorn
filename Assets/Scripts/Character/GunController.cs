3bd658224729af245548398b53bb435b668ba680
Jeremy Mariano <j.mariano@forsys.ch>
2018-05-23 11:07:29 +0200

    Refactoring

--- Assets/Scripts/Character/GunController.cs
+++ Assets/Scripts/Character/GunController.cs
@@ -1,35 +1,48 @@
-﻿ using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GunController : MonoBehaviour {
 
-    public GameObject bullet;
+    public GameObject bulletTemplate;
     public float firerate = 1;
-    private float shoot_timer = 0;
+    private float shootTimer = 0;
     public float explosion = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		if(Input.GetAxis("Fire1") > 0) {
-            if(shoot_timer <= 0) {
-                GetComponent<CameraShake>().Shake(.02f,0.2f);
-                GameObject b = (GameObject)Instantiate(bullet, transform.position, transform.rotation);
-                b.GetComponent<Bullet>().explosion = explosion;
-                b.transform.localScale *= GetComponentInParent<PlayerPlatformerController>().bulletSize;
-                b.transform.Rotate(new Vector3(0, 0, Random.Range(-2f, 2f)));
-                b.GetComponent<Bullet>().damage *= GetComponentInParent<PlayerPlatformerController>().bulletSize;
-                GetComponent<AudioSource>().Play();
-                GetComponentInParent<Rigidbody2D>().transform.Translate(-transform.right * .1f);
-                Destroy(b, 2f);
-                shoot_timer = 1 / (firerate * GetComponentInParent<PlayerPlatformerController>().shootSpeed);
-            }
+    private void Update()
+    {
+        // Update firerate timer
+        if (shootTimer > 0)
+        {
+            shootTimer -= Time.deltaTime;
         }
-        shoot_timer -= Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate ()
+    {
+        // If the firing button is used
+		if(Input.GetAxis("Fire1") > 0 && shootTimer <= 0)
+        {
+            // Shake camera
+            GetComponent<CameraShake>().Shake(.02f,0.2f);
+
+            // Fire a new bullet
+            GameObject newBullet = (GameObject)Instantiate(bulletTemplate, transform.position, transform.rotation);
+            newBullet.GetComponent<Bullet>().explosion = explosion;
+            newBullet.transform.localScale *= GetComponentInParent<PlayerPlatformerController>().bulletSize;
+            newBullet.transform.Rotate(new Vector3(0, 0, Random.Range(-2f, 2f)));
+            newBullet.GetComponent<Bullet>().damage *= GetComponentInParent<PlayerPlatformerController>().bulletSize;
+
+            // Play gun sound
+            GetComponent<AudioSource>().Play();
+
+            // Recoil
+            GetComponentInParent<Rigidbody2D>().transform.Translate(-transform.right * .1f);
+
+            // Destroy bullet after 2 seconds to prevent it to go indefinitely
+            Destroy(newBullet, 2f);
+
+            // Reset firerate timer
+            shootTimer = 1 / (firerate * GetComponentInParent<PlayerPlatformerController>().shootSpeed);
+        }
 	}
 }
